4be1d0a4f50fc6657198d2b39e46589f21d08ee7
castial <897257524@qq.com>
2017-03-07 19:23:42 +0800

    定义主色调

--- EasyMap/Expand/Components/HYNavigationController/HYNavigationController.m
+++ EasyMap/Expand/Components/HYNavigationController/HYNavigationController.m
@@ -18,16 +18,17 @@
     [super viewDidLoad];
     
     // 修改导航栏相关属性
-    UIColor *navTitleColor = [UIColor colorWithRed:0.322 green:0.322 blue:0.322 alpha:1.0f];    // 导航栏字体颜色
-    UIFont *navTitleFont = [UIFont boldSystemFontOfSize:16];    // 导航栏字体大小
+    UIColor *navTitleColor = [UIColor whiteColor];    // 导航栏字体颜色
+    UIFont *navTitleFont = [UIFont boldSystemFontOfSize:18];    // 导航栏字体大小
     
     [[UINavigationBar appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName: navTitleColor, NSFontAttributeName:navTitleFont}];
     [[UINavigationBar appearance] setTintColor:navTitleColor];
+    [[UINavigationBar appearance] setBarTintColor:[UIColor colorWithRed:58.0/255.0 green:155.0/255.0 blue:252.0/255.0 alpha:1.0f]];
     [[UINavigationBar appearance] setBackIndicatorImage:[UIImage imageNamed:@"btn_back@3x.png"]];
     [[UINavigationBar appearance] setBackIndicatorTransitionMaskImage:[UIImage imageNamed:@"btn_back@3x.png"]];
     
     // 修改UIBarButtonItem相关属性
-    [[UIBarButtonItem appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName : navTitleColor, NSFontAttributeName : [UIFont systemFontOfSize:14]} forState:UIControlStateNormal];
+    [[UIBarButtonItem appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName : navTitleColor, NSFontAttributeName : [UIFont systemFontOfSize:16]} forState:UIControlStateNormal];
     [[UIBarButtonItem appearance] setBackButtonTitlePositionAdjustment:UIOffsetMake(0, -60) forBarMetrics:UIBarMetricsDefault];
 }
 
